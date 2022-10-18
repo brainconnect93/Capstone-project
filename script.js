7adfce42122d99e489b5d8644abf69f59ede2700
brainconnect93 <brainconnect0@gmail.com>
2022-10-19 00:50:02 +0100

    Make changes

--- script.js
+++ script.js
@@ -84,24 +84,3 @@ for (let i = 0; i < featSpeaker.length; i += 1) {
 
   `;
 }
-
-const read = [
-  {
-    name: "Kilman Chon",
-    Image: "images/image-2.png",
-    title: "",
-    desc: "Kilman Chon helped bring the Internet to Asia and he is an outspoken advocate for the open web and digital commons in 2012, he was inducted into the inaugural class of the Internet",
-  },
-  {
-    name: "Julia Leda",
-    Image: "images/image-4.png",
-    title: "President of Young Pirates of Europe",
-    desc: "European ingetration, political democracy and participation of youth through online as her major condern, Reda report outlining potentials",
-  },
-  {
-    name: "Ryan Dyrud",
-    Image: "images/image-6.png",
-    title: "Berkman Professor of Enterpreneurial Legal Studies",
-    desc: "Benkler studies commons-based peer production, and published his seminal book The Wealth of Network in 2016",
-  },
-];
